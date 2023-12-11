ebac9168c7e416516d33764054c600bbfbe4cbf0
Danil Putintsev <workdanilput@mail.ru>
2023-12-11 16:20:33 +0300

    add RuntimeException

--- lab4/src/classes/Thingroof.java
+++ lab4/src/classes/Thingroof.java
@@ -6,7 +6,7 @@ public class Thingroof extends Thing {
     public Thingroof(String name, boolean is_swimming, int size){
         super(name, is_swimming, size);
     }
-    public static String toString(Thingroof... thingsroofs) throws RuntimeException{
+    public static String toString(Thingroof... thingsroofs) throws IndexOutException{
         String[] names = new String[thingsroofs.length];
         for (int i=0; i < thingsroofs.length; i++){
             String name = thingsroofs[i].getName();
