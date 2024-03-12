68353bbab48de91bdd55c30f0da06cb849e721fd
Danil <workdanilput@mail.ru>
2024-03-12 22:56:34 +0300

    release add_if_max and add_if_min

--- lab5/src/main/java/org/example/Commands/AddIfMaxCommand.java
+++ lab5/src/main/java/org/example/Commands/AddIfMaxCommand.java
@@ -1,12 +1,32 @@
 package org.example.Commands;
 
+import org.example.Collections.StudyGroup;
 import org.example.Interface.Command;
+
+import static org.example.Managers.CollectionManager.study_groups;
+import static org.example.Managers.CommandManager.element;
 
 public class AddIfMaxCommand implements Command {
     @Override
     public void execute(){
+        //Найти максимальный элемент в коллекции
+        System.out.println("Добавить элемент в коллекцию, если количество студентов в новой группе превышает количество людей в любой группе");
+        Integer max_students_count = 0;
+        for (StudyGroup group : study_groups) {
+            if (max_students_count < group.getStudentsCount()){
+                max_students_count = group.getStudentsCount();
+            }
+        }
 
-        System.out.println("Что-то с максимальным элементом");
+        //Прочитать элемент
+        StudyGroup group = element;
+        if (group.getStudentsCount() > max_students_count) {
+            study_groups.add(group);
+        }
+        else {
+            System.out.println("Не удалось добавить элемент в коллекцию. Группа не максимальная :(");
+        }
+
     }
 
     @Override
--- lab5/src/main/java/org/example/Commands/AddIfMinCommand.java
+++ lab5/src/main/java/org/example/Commands/AddIfMinCommand.java
@@ -1,11 +1,29 @@
 package org.example.Commands;
 
+import org.example.Collections.StudyGroup;
 import org.example.Interface.Command;
+
+import static org.example.Managers.CollectionManager.study_groups;
+import static org.example.Managers.CommandManager.element;
 
 public class AddIfMinCommand implements Command {
     @Override
     public void execute() {
-        System.out.println("Что-то с минимумом");
+        System.out.println("Добавить элемент в коллекцию, если количество студентов в введенной группе минимально");
+        //Нахождение минимального количества студентов
+        Integer min_students_count = 2099999999;
+        for (StudyGroup group : study_groups) {
+            if (group.getStudentsCount() < min_students_count) {
+                min_students_count = group.getStudentsCount();
+            }
+        }
+        StudyGroup group = element;
+        if (group.getStudentsCount() < min_students_count) {
+            study_groups.add(group);
+        }
+        else {
+            System.out.println("Не удалось добавить элемент в коллекцию. Группа не минимальная :(");
+        }
     }
 
     @Override
