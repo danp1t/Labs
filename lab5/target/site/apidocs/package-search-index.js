97ecb2c0621109cfba9e69bc958d3d9225dcaf54
Danil <workdanilput@mail.ru>
2024-03-15 17:58:39 +0300

    add docs

---
+++ lab5/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.Collections"},{"l":"org.example.Commands"},{"l":"org.example.Exceptions"},{"l":"org.example.Interface"},{"l":"org.example.Managers"}];updateSearchResults();
